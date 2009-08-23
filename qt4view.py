19ed668d807dcd128da5a386548f3dda1c2ff010
Lee Harr <missive@hotmail.com>
2009-08-23 11:20:46 -0400

    qt4view initial working state

--- qt4view.py
+++ qt4view.py
@@ -1,18 +1,23 @@
 import os
 import sys
+import math
+pi = math.pi
 from PyQt4 import QtCore, QtGui, QtSvg
 
+import main
+import world
 
-def setrend(app):
+
+def getrend(app):
     filename = 'robot.svg'
     filepath = os.path.join('data/images', filename)
     fp = QtCore.QString(filepath)
-    global rend
     rend = QtSvg.QSvgRenderer(fp, app)
+    return rend
 
 
 class MainWindow(QtGui.QMainWindow):
-    def __init__(self):
+    def __init__(self, app):
         QtGui.QMainWindow.__init__(self)
         #self.resize(800, 600)
 
@@ -28,16 +33,18 @@ class MainWindow(QtGui.QMainWindow):
         self.scene = Scene()
         self.setCentralWidget(self.scene.view)
 
-        self.startTimer(17)
+        self.startTimer(25)
 
-        self.rot = 0
-        self.pos = (0, 0)
-        self.turr_rot = 0
+        self.game = main.Game()
+        self.game.w.v.scene = self.scene
+        self.game.w.v.app = app
+        self.game.w.v.setrend()
+        self.game.load_robots()
 
     def timerEvent(self, ev):
-        self.tick()
+        self.game.tick()
 
-    def tick(self):
+    def test(self):
         self.scene.r.set_rotation(self.rot)
         self.scene.r.set_position(self.pos)
         self.scene.r.set_turr_rot(self.turr_rot)
@@ -86,15 +93,18 @@ class Scene(QtGui.QGraphicsScene):
         w.setBrush(wbrush)
         w.setPen(wpen)
 
-        r = Robot((100, 100), 30)
-        self.addItem(r)
-        self.r = r
-
         view = QtGui.QGraphicsView(self)
         view.resize(800, 600)
         view.show()
         view.scale(.9, .9)
         self.view = view
+
+size = 102
+def tl(pos):
+    px, py =  pos
+    sz = size / 2
+    x, y = (px*sz)-330, (py*sz)+5
+    return x, y
 
 class GraphicsItem(QtGui.QGraphicsItem):
     def __init__(self):
@@ -114,12 +124,12 @@ class GraphicsItem(QtGui.QGraphicsItem):
         trans.translate(cx, cy).rotate(ang).translate(-cx, -cy)
         self.item.setTransform(trans)
 
-    def set_position(self, pos):
+    def setpos(self, pos):
         self.pos = pos
         self.set_transform()
 
     def set_rotation(self, ang):
-        self.ang = ang
+        self.ang = -(180/pi)*ang
         self.set_transform()
 
     def rotate(self, deg):
@@ -129,9 +139,13 @@ class GraphicsItem(QtGui.QGraphicsItem):
     def paint(self, painter, option, widget):
         pass
 
+    def kill(self):
+        scene = self.item.scene()
+        scene.removeItem(self.item)
+
 class Robot(GraphicsItem):
     nrobots = 0
-    def __init__(self, pos, ang):
+    def __init__(self, pos, ang, rend):
         Robot.nrobots += 1
 
         self.pos = pos
@@ -146,8 +160,12 @@ class Robot(GraphicsItem):
         self.item.setSharedRenderer(rend)
         self.item.setElementId(imageid)
 
-        self.turr = Turret(self)
+        self.turr = Turret(self, rend)
 
+        self.set_transform()
+
+    def setpos(self, pos):
+        self.pos = tl(pos)
         self.set_transform()
 
     def boundingRect(self):
@@ -158,7 +176,7 @@ class Robot(GraphicsItem):
 
 
 class Turret(GraphicsItem):
-    def __init__(self, robot):
+    def __init__(self, robot, rend):
         self.pos = 50, 50
         self.ang = 0
         self.scale = 1
@@ -171,10 +189,98 @@ class Turret(GraphicsItem):
         self.item.setElementId('turret')
         self.set_transform()
 
+size2 = 20
+def tl2(pos):
+    px, py =  pos
+    sz = size2 / 2
+    x, y = (px*sz)-35, (py*sz)-15
+    print int(px), int(py), '->', int(x), int(y)
+    return x, y
 
-if __name__ == "__main__":
+class Bullet(GraphicsItem):
+    def __init__(self, pos):
+        self.pos = pos
+        self.ang = 0
+        self.scale = 1
+        self.cx, self.cy = 0, 0
+
+        x, y = tl2(pos)
+        self.item = QtGui.QGraphicsEllipseItem(x, y, 10, 10)
+        color = QtGui.QColor(90, 90, 70)
+        brush = QtGui.QBrush(color)
+        self.item.setBrush(brush)
+        self.set_transform()
+
+    def setpos(self, pos):
+        self.pos = tl2(pos)
+        self.set_transform()
+
+    def set_transform(self):
+        x, y = self.pos
+        trans = QtGui.QTransform()
+        trans.translate(x, y)
+        self.item.setTransform(trans)
+
+
+class Wall(object):
+    def __init__(self, pos, size):
+        pass
+
+class RobotInfo(object):
+    def __init__(self, n, name):
+        self.health = Health()
+
+class Health(object):
+    def step(self, n=None):
+        pass
+
+class Explosion(object):
+    def __init__(self, a=None, b=None):
+        pass
+
+    def setpos(self, pos):
+        pass
+
+    def set_rotation(self, ang):
+        pass
+
+    def kill(self):
+        pass
+
+class Arena(object):
+    def setrend(self):
+        self.rend = getrend(self.app)
+
+    def addrobot(self, pos, ang):
+        v = Robot(pos, ang, self.rend)
+        self.scene.addItem(v.item)
+        #self.scene.addItem(v.turr.item)
+        return v
+
+    def addrobotinfo(self, n, name):
+        return RobotInfo(n, name)
+
+    def addbullet(self, pos):
+        v = Bullet(pos)
+        self.scene.addItem(v.item)
+        return v
+
+    def addexplosion(self, pos):
+        e = Explosion(pos)
+        return e
+
+    def step(self, x=None):
+        pass
+
+
+def run():
     app = QtGui.QApplication(sys.argv)
-    setrend(app)
-    win = MainWindow()
+    import qt4view
+    world.view = qt4view
+    win = MainWindow(app)
     win.show()
     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    run()
