d3a78431003762a5f6e588ce27b290824662a73b
Lee Harr <missive@hotmail.com>
2012-05-28 10:10:08 -0400

    make about screen easier to maintain

---
+++ about.py
@@ -0,0 +1,48 @@
+# Copyright 2010 Lee Harr
+#
+# This file is part of pynguin.
+# http://pynguin.googlecode.com/
+#
+# Pynguin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pynguin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pynguin.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import os
+
+from PyQt4 import QtGui, QtCore, uic
+
+import util
+version = 'pybotwar-0.8'
+
+datadir = 'data'
+uidir = os.path.join(datadir, 'ui')
+
+class AboutDialog(QtGui.QDialog):
+    def __init__(self, app):
+        QtGui.QDialog.__init__(self)
+        uifile = 'about.ui'
+        uipath = os.path.join(uidir, uifile)
+        self.ui = uic.loadUi(uipath, self)
+
+        svgrenderer = util.SvgRenderer(app)
+        rend = svgrenderer.getrend()
+        img = QtGui.QPixmap(200, 100)
+        img.fill(QtCore.Qt.transparent)
+        self.img = img
+        painter = QtGui.QPainter(img)
+        rend.render(painter, 'splash')
+        painter.end()
+
+        self.ui.splasharea.setPixmap(self.img)
+        self.ui.progtitle.setText(version)
+
