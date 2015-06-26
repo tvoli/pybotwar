a2c7d2e65aaf5002bdf2c971abd933803a740e02
Jesper Öman <jo@magine.com>
2015-06-26 14:40:13 +0200

    Remove log

--- robots/sharkbot.py
+++ robots/sharkbot.py
@@ -3,7 +3,5 @@ from robot import Robot
 class TheRobot(Robot):
     def respond(self):
         self.torque(50)
-        self.log(sensors['TICK'])
         if self.sensors['TICK'] % 10 == 0:
             self.fire()
-
