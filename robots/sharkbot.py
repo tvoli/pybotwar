8098a0b51bd653e2305736d588a97534635852ed
Andreas Launila <andreas.launila@magine.com>
2015-06-26 14:57:18 +0200

    Misc changes

--- robots/sharkbot.py
+++ robots/sharkbot.py
@@ -1,8 +1,26 @@
 from robot import Robot
 
 class TheRobot(Robot):
+    def initialize(self):
+        self.act_next = 0
+
     def respond(self):
-        self.torque(50)
-        self.force(40)
+        kind, angle, dist = self.sensors['PING']
+        if self.sensors['TICK'] > self.act_next:
+            self.torque(50)
+            self.turret(40)
+            self.force(40)
+        else:
+            self.turret(0)
+            self.torque(0)
+            self.force(0)
+
+        if kind == 'r':
+            self.log('Wait')
+            self.act_next = self.sensor['TICK'] + 10000
+
+        self.log(kind)
         if self.sensors['TICK'] % 10 == 0:
             self.fire()
+        self.ping()
+
