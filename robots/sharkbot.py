5febe2c206b659a53ffbcb8072dda565a5f08620
Andreas Launila <andreas.launila@magine.com>
2015-06-26 14:46:13 +0200

    Add some movement

--- robots/sharkbot.py
+++ robots/sharkbot.py
@@ -3,5 +3,6 @@ from robot import Robot
 class TheRobot(Robot):
     def respond(self):
         self.torque(50)
+        self.force(40)
         if self.sensors['TICK'] % 10 == 0:
             self.fire()
